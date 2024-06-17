2577ec8e7f3963a0c27d0b38288cbea7ecab7e7f
riquelmeestudante <117414312+riquelmeestudante@users.noreply.github.com>
2024-06-17 10:50:12 -0300

    1

---
+++ scrip.js
@@ -0,0 +1,25 @@
+const caixaPrincipal = document.querySelector(".caixa.principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:   "Pergunta 1",
+        alternativas:  [
+            "Alternativas 1",
+            "Aternativa 2"
+        ]
+    }
+    {
+]
+    {
+    enunciado:   "Pergunta 1",
+    alternativas:  [
+        "Alternativas 1",
+        "Aternativa 2"
+    ]
+}
+    "Pergunta 2"
+]
